7a7c49ba48833691e494d398836c99c23ed6b85e
SHADOW <505029990@qq.com>
2025-12-05 11:53:08 +0800

    Updates default preVerificationGas and disables auto-estimation

--- src/app/hooks/web3/useAbstractAccount.ts
+++ src/app/hooks/web3/useAbstractAccount.ts
@@ -217,7 +217,7 @@ export const useAbstractAccount = (aaAddress: Address, chainId?: number) => {
         callData,
         callGasLimit: BigInt(21000),
         verificationGasLimit: BigInt(100_000),
-        preVerificationGas: BigInt(50_000),
+        preVerificationGas: BigInt(62_000),
         maxFeePerGas: BigInt(1),
         maxPriorityFeePerGas: BigInt(0),
         paymasterAndData: '0x' as Hex,
@@ -229,7 +229,7 @@ export const useAbstractAccount = (aaAddress: Address, chainId?: number) => {
       }
 
       const estimatedGas = await estimateUserOperationGas(userOp);
-      userOp.preVerificationGas = BigInt(estimatedGas.preVerificationGas);
+      // userOp.preVerificationGas = BigInt(estimatedGas.preVerificationGas);
       userOp.verificationGasLimit = chainConfig.supportPassKeySign
         ? (BigInt(estimatedGas.verificationGasLimit) * BigInt(12)) / BigInt(10)
         : BigInt(1000000); // maxVerificationGas of 3000000
